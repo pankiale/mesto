389d6e6ddf60a73878826b3c3921d7cf3b37095e
Alexander Pankin <alexander.g.pankin@gmail.com>
2021-12-22 12:25:24 +0100

    first version

---
+++ script.js
@@ -0,0 +1,37 @@
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let submitForm = document.querySelector('.form');
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.form__item_prof_title');
+let jobInput = document.querySelector('.form__tiem_prof_subtitle');
+
+editButton.addEventListener('click', function () {
+    popup.classList.add('popup_open');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+});
+
+
+closeButton.addEventListener('click', function () {
+    popup.classList.remove('popup_open');
+});
+
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popup.classList.remove('popup_open');
+}
+
+submitForm.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
