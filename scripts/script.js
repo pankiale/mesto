6419f522f90dbfaad2df039bcb26c178ba0096d0
Alexander Pankin <alexander.g.pankin@gmail.com>
2022-01-06 18:37:46 +0100

    review

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+//я понял коппентарии по скрипту, я перепишу, хочу сначала закрыть вопросы по верстке и стилям
+
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let submitForm = document.querySelector('.form');
@@ -8,27 +10,24 @@ let nameInput = document.querySelector('.form__item_prof_title');
 let jobInput = document.querySelector('.form__item_prof_subtitle');
 
 editButton.addEventListener('click', function () {
-    popup.classList.add('popup_open');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_open');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 });
 
 
 closeButton.addEventListener('click', function () {
-    popup.classList.remove('popup_open');
+  popup.classList.remove('popup_open');
 });
 
 function formSubmitHandler(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popup.classList.remove('popup_open');
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popup.classList.remove('popup_open');
 }
 
 submitForm.addEventListener('submit', formSubmitHandler);
-
-
-
 
 
 
